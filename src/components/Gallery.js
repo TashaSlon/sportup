3399f3a2317e0eed8210eede9d2259492888b56b
plyusnina <plyusnina@live.ru>
2023-10-17 16:52:47 +0500

    src image fix

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { TranslationContext } from '../contexts/translation/translationContext';
 
 import Step1 from '../images/deleteAccount1.jpg';
@@ -8,23 +8,22 @@ import Step4 from '../images/deleteAccount4.jpg';
 
 export default function Gallery() {
     const text = useContext(TranslationContext);
-    const activeImage = document.querySelector(".gallery__active");
-    console.log(activeImage);
+
+    const [src, setSrc] = useState(Step1);
+    const [altImage, setAltImage] = useState(text.deleteAccountImage1);
+
     const productImages = document.querySelectorAll(".gallery__figure");
 
     function changeImage(e) {
-        const img = e.target.previousSibling;
-        console.log(e.target.previousSibling.src);
-        console.log(img);
-        console.log(activeImage);
-        activeImage.src = img.src;
+        setSrc(e.target.previousSibling.src);
+        setAltImage(e.target.previousSibling.alt);
     }
 
     productImages.forEach(image => image.addEventListener("click", changeImage));
 
     return (
         <div className="gallery">
-            <img className="gallery__active gallery__image" src={Step2} alt={text.deleteAccountImage1}/>
+            <img className="gallery__active gallery__image" src={src} alt={altImage}/>
             <ul className="gallery__image-list">
                 <li className="gallery__image-item">
                     <figure className="gallery__figure" onClick={changeImage}>
