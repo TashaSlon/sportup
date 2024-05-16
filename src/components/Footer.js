c151652bf180bd1074ee568ac2402b1f1491fc42
plyusnina <plyusnina@live.ru>
2024-05-16 14:56:21 +0500

    INN for Russian

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,14 @@ export default function Footer() {
     return (
         <footer className="footer">
                 <div className="footer__block">
-                    <p className='footer__copiright'>&copy; {year} SportUp</p>
+                    {(localStorage.getItem('lang') == 'ru') ? 
+                        <div>
+                            <p className='footer__copiright'>&copy; {year} ООО "МАД РЕДИС Лаб"</p>
+                            <p className='footer__text'>6140051 Пермь Уинская 4</p>
+                            <p className='footer__text'>ИНН: 5906174862 КПП:590601001</p>
+                        </div>
+                    : <p className='footer__copiright'>&copy; {year} SportUp</p>
+                    }
                 </div>
         </footer>
     );
